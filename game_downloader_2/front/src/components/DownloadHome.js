f5775c790e3bd0f19759a56b20503ada93f575eb
raginisharma1999 <43653520+raginisharma1999@users.noreply.github.com>
2020-09-23 18:26:42 +0530

    download v2

--- game_downloader_2/front/src/components/DownloadHome.js
+++ game_downloader_2/front/src/components/DownloadHome.js
@@ -15,8 +15,8 @@ const App = () => {
           <div className="row">
             <div className="col col-12">
             <header className="App-header">
-              <h1 className="display-4 mt-4 mb-4">{t("Download the game from here!")}</h1>
-              <p>{t("Kindly choose one of the options")}</p>
+              <h1 className="display-4 mt-4 mb-4">{t("Download Adamya")}</h1>
+              {/* <p>{t("Kindly choose one of the options")}</p> */}
             </header>
             <main>
                 <a target="_blank" href="http://localhost:5027/windows">
